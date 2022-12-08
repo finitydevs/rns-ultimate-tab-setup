764f20107c42c800f38343de520c7303b806871e
Mayer Lench <mlench@finitydevelopment.com>
2022-12-07 23:47:29 -0500

    adds android status bar height

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,6 +1,7 @@
 import { BaseNavigationContainer } from '@react-navigation/core';
 import * as React from "react";
 import { stackNavigatorFactory } from "react-nativescript-navigation";
+import { getStatusBarHeight } from '../utils';
 import Home from './Home/Home';
 import HomeBottomNav from './Home/HomeBottomNav';
 import LaunchScreen from './LaunchSreen';
@@ -16,12 +17,12 @@ export const Navigator = () => {
         <BaseNavigationContainer>
             <StackNavigator.Navigator
                 initialRouteName="LaunchScreen"
-
                 screenOptions={{
                     gestureEnabled: true,
                     headerStyle: {
                         backgroundColor: "white",
                     },
+                    contentStyle: { marginTop: getStatusBarHeight() },
                     headerShown: false,
                 }}
             >
